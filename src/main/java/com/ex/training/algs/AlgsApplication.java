c09c38dd1e2ed109fd953be100a9bbd501a7ff8f
Luis Cortes <lfcortes12@gmail.com>
2019-10-05 07:08:12 -0500

    adjust main class to read program argument to get algorithm to be executed

--- src/main/java/com/ex/training/algs/AlgsApplication.java
+++ src/main/java/com/ex/training/algs/AlgsApplication.java
@@ -15,31 +15,47 @@ import lombok.extern.slf4j.Slf4j;
 public class AlgsApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		
 		SpringApplication.run(AlgsApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
 		var scanner = new Scanner(System.in);
+		
+		switch (AlgorithmName.valueOf(args[0])) {
+		case QUICK_FIND:
+			runQuickFind(scanner);
+			break;
+		default:
+			break;
+		}
+		
+		
+
+	}
+
+	private void runQuickFind(Scanner scanner) {
 		log.info("Size: ");
 		int n = scanner.nextInt();
 		final QuickFindUF quickFindUF = new QuickFindUF(n);
 		log.info("Array created {}", quickFindUF.toString());
-		while(scanner.hasNext()) {
+		String line = "";
+		while (!line.equalsIgnoreCase("q")) {
+			log.info("Nodes to join?");
 			int p = scanner.nextInt();
 			int q = scanner.nextInt();
-			
-			if (quickFindUF.connected(p, q)) continue;
+
+			if (quickFindUF.connected(p, q))
+				continue;
 			quickFindUF.union(p, q);
-            log.info("p {} q {}", p, q);
+			log.info("p {} q {}", p, q);
+			log.info("Union {}", quickFindUF.toString());
+			log.info("To continue press C otherwise Q");
+			line = scanner.next();
 		}
-		
-		
-		quickFindUF.union(2, 3);
+
 		log.info("Union {}", quickFindUF.toString());
 		scanner.close();
-		
 	}
 
 }
--- src/main/java/com/ex/training/algs/quickfind/QuickFindUF.java
+++ src/main/java/com/ex/training/algs/quickfind/QuickFindUF.java
@@ -3,7 +3,7 @@ package com.ex.training.algs.quickfind;
 import java.util.Arrays;
 
 public class QuickFindUF {
-	
+
 	private int[] id;
 
 	public QuickFindUF(int size) {
@@ -12,22 +12,22 @@ public class QuickFindUF {
 			id[i] = i;
 		}
 	}
-	
+
 	public boolean connected(int p, int q) {
 		return id[p] == id[q];
 	}
-	
+
 	public void union(int p, int q) {
 		int pValue = id[p];
 		int qValue = id[q];
-		
-		for (int i : id) {
-			if(id[i] == pValue) {
+
+		for (int i = 0; i < id.length; i++) {
+			if (id[i] == pValue) {
 				id[i] = qValue;
 			}
 		}
 	}
-	
+
 	public String toString() {
 		return Arrays.toString(id);
 	}
